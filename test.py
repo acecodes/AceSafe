a5fa018568e5dff807494a540e6dc3b42575ae10
Ace <ace.github@gmail.com>
2014-06-05 19:08:15 -0700

    Started the process of switching to SQLAlchemy - still in development

--- test.py
+++ test.py
@@ -2,20 +2,41 @@ import unittest
 from os import system
 from dirobj import DirObject
 from getpass import getuser
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
 
 user = getuser()
 
 testdir = "/home/%s/dev/testing" % (user)
 
-class Tester(unittest.TestCase):
+Base = declarative_base()
 
-	def test_db_build(self):
-		system("""python3 my_build.py""")
+engine = create_engine('sqlite:///test.db', echo=True)
 
-	def test_compare_dirs(self):
-		DirTest1 = DirObject('Test1', "/home/%s/dev/testing/1" % (user))
-		DirTest2 = DirObject('Test2', "/home/%s/dev/testing/2" % (user))
-		DirTest1.routine(DirTest2)
+class Routine(Base):
+	# Placeholder because SQLAlchemy requires that this be declared for any ORM class
+	__tablename__ = 'Rename This'
+
+	def __init__(self, name):
+		self.name = name
+		# Name variable replaces the 'Replace This' above with a new name entered at instantiation time
+		__tablename__ = name
+
+	Routines = Column(String, primary_key=True)
+
+	def __repr__(self):
+		return "Routine Table Name: {0}".format(self.name)
+
+	def add_routines(self, *routines):
+		session = Session()
+		# Add code for inserting routines into database here
+
+
+
+#class Tester(unittest.TestCase):
+
+Dropbox = Routine('Dropbox')
 
 if __name__ == '__main__':
-	unittest.main()
+	#unittest.main()
+	print(Dropbox)
