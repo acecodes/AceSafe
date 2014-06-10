39beb42bea3f72ea18ee302f1cb977cccf66e0ab
Ace <ace.github@gmail.com>
2014-06-10 16:59:21 -0700

    Fixed some error issues, primarily with handling exceptions when directories are not found

--- acesafe.py
+++ acesafe.py
@@ -72,12 +72,15 @@ if argv[1] == 'help':
 if argv[1] == 'dirs':
     src = DirObject('source', argv[2])
     dst = DirObject('destination', argv[3])
-    print('\nCopying files from {0} to {1}...'.format(src.src, dst.src))
-    try:
-        input()
-    except SyntaxError:
-        pass
-    src.copy_dirs(dst.src)
+    if os.path.exists(argv[2]) and os.path.exists(argv[3]):
+        print('\nCopying files from {0} to {1}...'.format(src.src, dst.src))
+        try:
+            input()
+        except SyntaxError:
+            pass
+        src.copy_dirs(dst.src)
+    else:
+        print('Those paths are invalid, please try again.')
     exit()
 
 
